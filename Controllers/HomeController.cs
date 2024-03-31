949e349b94b71adf7b56c2114403efa0877d978b
s3826714 <67183686+s3826714@users.noreply.github.com>
2024-03-31 12:41:03 +1100

    Update home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,25 +8,22 @@ namespace PatientConnect.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // Home page
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
+        // About us page
+        public IActionResult AboutUs() => View();
+
+        // How it works page
+        public IActionResult HowItWorks() => View();
+
+        // Contact page
+        public IActionResult Contact() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
