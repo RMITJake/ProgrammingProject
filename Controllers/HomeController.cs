4083bce2db1f774774fd32d46b45015b14847274
s3826714 <67183686+s3826714@users.noreply.github.com>
2024-03-31 13:34:40 +1100

    Layout and Home controller pages cleaned up and images added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,28 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 using PatientConnect.Models;
 using System.Diagnostics;
 
-namespace PatientConnect.Controllers
+namespace PatientConnect.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) => _logger = logger;
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
-        // Home page
-        public IActionResult Index() => View();
+    // Home page
+    public IActionResult Index() => View();
 
+    // About us page
+    public IActionResult AboutUs() => View();
 
-        // About us page
-        public IActionResult AboutUs() => View();
+    // How it works page
+    public IActionResult HowItWorks() => View();
 
-        // How it works page
-        public IActionResult HowItWorks() => View();
+    // Contact page
+    public IActionResult Contact() => View();
 
-        // Contact page
-        public IActionResult Contact() => View();
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PatientConnect.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index() => View();
+
+    public IActionResult Register() => View();
+}
