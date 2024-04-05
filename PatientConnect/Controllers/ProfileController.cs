a61cd97a6d5b37a2064a8221856d3bcd0e6f5d7e
harrisontang <S3908223@student.rmit.edu.au>
2024-04-05 19:36:23 +1000

    User profile update
    - Added profileController for viewing profile
    - Added profile index.cshtml for profile controller index view
    - Altered layout to show login, register on no session; show logout, profile on set session (session set on UserID stored in session int32 property)

---
+++ PatientConnect/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientConnect.Models;
+using PatientConnect.Data;
+
+namespace PatientConnect.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly PatientConnectContext _context;
+        private int UserID => HttpContext.Session.GetInt32("UserID").Value;
+
+        public ProfileController(PatientConnectContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.Users.FindAsync(UserID));
+        }
+
+        // public async Task<IActionResult> ChangePassword() {}
+    }
+}
