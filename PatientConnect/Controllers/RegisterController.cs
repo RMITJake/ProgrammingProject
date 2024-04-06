29a3f142b5230a87a707500c8ee0200c1d6cc3a8
RMITJake <s3905550@student.rmit.edu.au>
2024-04-06 13:26:32 +1100

    Started implementing SMTP service

--- PatientConnect/Controllers/RegisterController.cs
+++ PatientConnect/Controllers/RegisterController.cs
@@ -3,13 +3,14 @@ using PatientConnect.Data;
 using PatientConnect.DTOs;
 using PatientConnect.Models;
 using SimpleHashing.Net;
+using System.Net;
+using System.Net.Mail;
 
 
 namespace PatientConnect.Controllers;
 
 // Bonus Material: Implement global authorisation check.
 //[AllowAnonymous]
-[Route("/Register")]
 public class RegisterController : Controller
 {
     private static readonly ISimpleHash s_simpleHash = new SimpleHash();
@@ -18,9 +19,11 @@ public class RegisterController : Controller
 
     public RegisterController(PatientConnectContext context) => _context = context;
 
+    [Route("/Register")]
     public IActionResult Register() => View();
 
     [HttpPost]
+    [Route("/Register")]
     public IActionResult Register(UserDto dto)
     {
         if (ModelState.IsValid)
@@ -34,6 +37,21 @@ public class RegisterController : Controller
                 PostCode = dto.PostCode,
                 UserType = dto.UserType
             };
+
+            SmtpClient smtpClient = new SmtpClient("smtp.elasticemail.com"){};
+            smtpClient.Port = 2525;
+            smtpClient.Credentials = new NetworkCredential ("patientconnect@protonmail.com", "2C9A9B05319A440E34267209C87615F5E661");
+            smtpClient.EnableSsl = true;
+
+            MailMessage message = new MailMessage{
+                From = new MailAddress("patientconnect@protonmail.com"),
+                Subject = "Patient Connect Registration Confirmation",
+                Body = "Thank you " + dto.Name + " for registering with Patient Connect!"
+            };
+
+            message.To.Add(dto.Email);
+
+            smtpClient.Send(message);
 
             return RedirectToAction("RegistrationSuccess");
         }   
