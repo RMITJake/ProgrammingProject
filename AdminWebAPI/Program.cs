3eb1611add46e78b89443673be0f0400896c0ea9
s3826714 <67183686+s3826714@users.noreply.github.com>
2024-05-18 18:10:25 +1000

    changes to appsettings and fixing errors

--- AdminWebAPI/Program.cs
+++ AdminWebAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<PatientConnectContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString(nameof(McbaContext))));
+    options.UseSqlServer(builder.Configuration.GetConnectionString(nameof(PatientConnectContext))));
 
 builder.Services.AddScoped<UserManager>();
 
