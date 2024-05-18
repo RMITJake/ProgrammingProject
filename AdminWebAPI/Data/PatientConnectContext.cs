9fd90d4b793059e16ddeb820f86b063433deb2b6
s3826714 <67183686+s3826714@users.noreply.github.com>
2024-05-18 18:05:26 +1000

    Setting up the Admin Project

---
+++ AdminWebAPI/Data/PatientConnectContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdminWebAPI.Models;
+
+namespace AdminWebAPI.Data;
+
+public class PatientConnectContext : DbContext
+{
+    public PatientConnectContext(DbContextOptions<PatientConnectContext> options) : base(options)
+    { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Login> Logins { get; set; }
+    public DbSet<Connection> Connections { get; set; }
+}
---
+++ AdminWebAPI/Program.cs
@@ -0,0 +1,49 @@
+using AdminWebAPI.Data;
+using AdminWebAPI.Managers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<PatientConnectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(nameof(McbaContext))));
+
+builder.Services.AddScoped<UserManager>();
+
+// Configure the default client.
+builder.Services.AddHttpClient(Options.DefaultName, client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5100");
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+    app.UseExceptionHandler("/Home/Error");
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.UseSession();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Index}/{id?}");
+
+app.Run();
